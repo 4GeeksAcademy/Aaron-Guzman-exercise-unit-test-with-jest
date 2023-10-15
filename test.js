61e5d8177c8736a5ef6677c227edf9e6f91a5159
Aarón Guzmán <140634896+aaronguf@users.noreply.github.com>
2023-10-15 00:36:11 +0000

    Mi primer test de funciones by AaronGuf

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 
 
-test("One euro should be 1.206 dollars", function(){
+test("One euro should be 1.2 dollars", function(){
     // importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js')
     
     // hago mi comparacion (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    expect(fromEuroToDollar(1)).toBe(1.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
 })
 
 
@@ -14,7 +14,7 @@ test("One dollar should be 106.5833 yens", function(){
     const { fromDollarToYen } = require('./app.js')
     
     // hago mi comparacion (la prueba)
-    expect(fromDollarToYen(1)).toBe(106.5833); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    expect(fromDollarToYen(1)).toBeCloseTo(106.5833, 4); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
 })
 
 
@@ -23,5 +23,13 @@ test("One yen should be 0.00625 pounds", function(){
     const { fromYenToPuond } = require('./app.js')
     
     // hago mi comparacion (la prueba)
-    expect(fromYenToPuond(500)).toBe(3.127); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    expect(fromYenToPuond(1)).toBeCloseTo(0.00625, 4); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+})
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
 })
